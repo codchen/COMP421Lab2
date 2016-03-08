88055db4ceaff41c4fc23a2b1040d4bcb063b20e
Tony <xc12@rice.edu>
2016-03-07 21:38:27 -0600

    initialize free frame list

--- yalnix.c
+++ yalnix.c
@@ -1,4 +1,7 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
 #include <comp421/yalnix.h>
 #include <comp421/hardware.h>
 
@@ -21,7 +24,18 @@ void trap_math_handler(ExceptionStackFrame *frame){}
 void trap_tty_receive_handler(ExceptionStackFrame *frame){}
 void trap_tty_transmit_handler(ExceptionStackFrame *frame){}
 
-extern void KernelStart(ExceptionStackFrame * frame, unsigned int pmem_size, void *orig_brk, char **cmd_args) {
+void *p_brk = 0;
+void *p_base = 0;
+void *p_limit = 0;
+char v_enabled = 0;
+int free_pf_head = -1;
+int free_pf_tail = -1;
+
+extern void KernelStart(ExceptionStackFrame * frame, 
+	unsigned int pmem_size, void *orig_brk, char **cmd_args) {
+	p_brk = orig_brk;
+	p_base = orig_brk;
+	p_limit = (void *)((long)(PMEM_BASE + pmem_size));
 	//initialize interrupt vector table
 	trap_handler *interrupt_vector_table = malloc(sizeof(trap_handler) * TRAP_VECTOR_SIZE);
 	if (interrupt_vector_table == NULL) return;
@@ -38,8 +52,31 @@ extern void KernelStart(ExceptionStackFrame * frame, unsigned int pmem_size, voi
 	interrupt_vector_table[TRAP_TTY_TRANSMIT] = trap_tty_transmit_handler;
 	WriteRegister(REG_VECTOR_BASE, (RCS421RegVal)interrupt_vector_table);
 
+	//TODO: initialize page tables
+	
+	//initialize free physical page list
+	//TODO: check if free space doesn't make up for even one single page
+	free_pf_head = UP_TO_PAGE(p_brk) >> PAGESHIFT;
+	for (i = free_pf_head; i < (DOWN_TO_PAGE(p_limit) >> PAGESHIFT) - 1; i++) {
+		*(long *)((long)i * PAGESIZE) = i + 1;
+	}
+	*(long *)((long)i * PAGESIZE) = MEM_INVALID_PAGES + 1;
+	for (i = MEM_INVALID_PAGES + 1; i < (DOWN_TO_PAGE(KERNEL_STACK_BASE) >> PAGESHIFT) - 1; i++) {
+		*(long *)((long)i * PAGESIZE) = i + 1;
+	}
+	free_pf_tail = i;
 }
 
 extern int SetKernelBrk(void *addr) {
+	if (!v_enabled) {
+		if (addr >= p_base && addr <= p_limit) {
+			p_brk = addr;
+			return 0;
+		}
+		else {
+			perror("Brk out of bound.");
+			return -1;
+		}
+	}
 	return 0;
 }
