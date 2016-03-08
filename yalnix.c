5ff2c1984bbbe08aba11045b668046cc20e5fd0b
Tony <xc12@rice.edu>
2016-03-08 09:13:01 -0600

    initialize page tables

--- yalnix.c
+++ yalnix.c
@@ -24,25 +24,22 @@ void trap_math_handler(ExceptionStackFrame *frame){}
 void trap_tty_receive_handler(ExceptionStackFrame *frame){}
 void trap_tty_transmit_handler(ExceptionStackFrame *frame){}
 
-void *p_brk = 0;
-void *p_base = 0;
-void *p_limit = 0;
+void *p_k_brk = 0;
+void *p_k_base = 0;
+void *p_k_limit = 0;
 char v_enabled = 0;
 int free_pf_head = -1;
 int free_pf_tail = -1;
+int free_pfn = 0;
 
 extern void KernelStart(ExceptionStackFrame * frame, 
 	unsigned int pmem_size, void *orig_brk, char **cmd_args) {
-	p_brk = orig_brk;
-	p_base = orig_brk;
-	p_limit = (void *)((long)(PMEM_BASE + pmem_size));
+	p_k_brk = orig_brk;
+	p_k_base = orig_brk;
+	p_k_limit = (void *)((long)(PMEM_BASE + pmem_size));
 	//initialize interrupt vector table
-	trap_handler *interrupt_vector_table = malloc(sizeof(trap_handler) * TRAP_VECTOR_SIZE);
+	trap_handler *interrupt_vector_table = calloc(TRAP_VECTOR_SIZE, sizeof(trap_handler));
 	if (interrupt_vector_table == NULL) return;
-	int i;
-	for (i = 0; i < TRAP_VECTOR_SIZE; i++) {
-		interrupt_vector_table = NULL;
-	}
 	interrupt_vector_table[TRAP_KERNEL] = trap_kernel_handler;
 	interrupt_vector_table[TRAP_CLOCK] = trap_clock_handler;
 	interrupt_vector_table[TRAP_ILLEGAL] = trap_illegal_handler;
@@ -52,25 +49,43 @@ extern void KernelStart(ExceptionStackFrame * frame,
 	interrupt_vector_table[TRAP_TTY_TRANSMIT] = trap_tty_transmit_handler;
 	WriteRegister(REG_VECTOR_BASE, (RCS421RegVal)interrupt_vector_table);
 
-	//TODO: initialize page tables
-	
+	//initialize page tables
+	void *region0 = calloc(PAGE_TABLE_LEN, sizeof(struct pte));
+	if (region0 == NULL) return;
+	WriteRegister(REG_PTR0, (RCS421RegVal)region0);
+	void *region1 = calloc(PAGE_TABLE_LEN, sizeof(struct pte));
+	if (region1 == NULL) return;
+	WriteRegister(REG_PTR1, (RCS421RegVal)region1);
+
 	//initialize free physical page list
 	//TODO: check if free space doesn't make up for even one single page
-	free_pf_head = UP_TO_PAGE(p_brk) >> PAGESHIFT;
-	for (i = free_pf_head; i < (DOWN_TO_PAGE(p_limit) >> PAGESHIFT) - 1; i++) {
+	free_pf_head = UP_TO_PAGE(p_k_brk) >> PAGESHIFT;
+	int i;
+	for (i = free_pf_head; i < (DOWN_TO_PAGE(p_k_limit) >> PAGESHIFT) - 1; i++) {
 		*(long *)((long)i * PAGESIZE) = i + 1;
+		free_pfn++;
 	}
 	*(long *)((long)i * PAGESIZE) = MEM_INVALID_PAGES + 1;
+	free_pfn++;
 	for (i = MEM_INVALID_PAGES + 1; i < (DOWN_TO_PAGE(KERNEL_STACK_BASE) >> PAGESHIFT) - 1; i++) {
 		*(long *)((long)i * PAGESIZE) = i + 1;
+		free_pfn++;
 	}
 	free_pf_tail = i;
+	free_pfn++;
+
+	//enable VM
+	WriteRegister(REG_VM_ENABLE, 1);
+
+	//TODO: idle
+
+
 }
 
 extern int SetKernelBrk(void *addr) {
 	if (!v_enabled) {
-		if (addr >= p_base && addr <= p_limit) {
-			p_brk = addr;
+		if (addr >= p_k_base && addr <= p_k_limit) {
+			p_k_brk = addr;
 			return 0;
 		}
 		else {
